ddf7135e4f27fed27e77191a6b87d5c2bf9d9858
valnative <valerii.yevdochenko@gmail.com>
2022-05-22 16:51:55 +0300

    Create product component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './styles.css'
 
+import Product from './components/Product'
+
 const app = createApp({})
+
+app.component('product', Product)
 
 app.mount('#app')
 app.mount('#app-cart')
