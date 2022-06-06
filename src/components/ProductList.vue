af8e2169e8fcce00f94bc3fe76b29097328fa948
valnative <valerii.yevdochenko@gmail.com>
2022-06-06 18:44:46 +0300

    Add data to ProductList.vue

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-      <product></product>
+      <product
+          name="Beanie"
+          v-bind:price="19.00"
+      ></product>
+
     </div>
   </div>
 </template>
