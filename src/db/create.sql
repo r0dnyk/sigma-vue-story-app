1a8a82241d0d79dd768428b7a112ff89877cef13
valnative <valerii.yevdochenko@gmail.com>
2022-06-09 14:05:08 +0300

    Add 'count' in sql queries

--- src/db/create.sql
+++ src/db/create.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `product`
 (
-    `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`        VARCHAR(255) NOT NULL,
-    `price`       DECIMAL      NOT NULL,
-    `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`  TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `image`       VARCHAR(255) NOT NULL,
+    `id`         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name`       VARCHAR(255) NOT NULL,
+    `price`      DECIMAL      NOT NULL,
+    `count`      SMALLINT UNSIGNED,
+    `image`      VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
