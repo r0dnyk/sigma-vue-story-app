e874affe00b15fdc770a3ecd37c4697c3e70a41d
valnative <valerii.yevdochenko@gmail.com>
2022-06-26 15:53:11 +0300

    Change index.php directory

--- framework/config.php
+++ framework/config.php
@@ -8,7 +8,7 @@ const APP_VIEWS = APP_ROOT . '/views/';
 const ROUTES_PATH = APP_ROOT . '/framework/routes.php';
 
 const DB_HOST = 'localhost';
-const DB_NAME = 'task5';
-const DB_USER = 'admin';
-const DB_PASS = '@secret@';
+const DB_NAME = 'sigma_task_5';
+const DB_USER = 'root';
+const DB_PASS = 'root';
 const DB_DSN = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
